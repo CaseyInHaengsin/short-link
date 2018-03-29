b66046ec776feca1cbd7ee1f74222c2924d68ec4
CaseyInHaengsin <casey.rowley89@gmail.com>
2018-03-29 07:18:33 -0600

    decrement challenge finished.

--- imports/ui/Signup.js
+++ imports/ui/Signup.js
@@ -3,12 +3,34 @@ import { Link } from 'react-router-dom';
 
 
 export default class Signup extends React.Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+          count: 0
+        };
+    }
+    increment(){
+        this.setState({
+            count: this.state.count + 1
+        });
+    }
+    decrement(){
+        this.setState({
+           count: this.state.count -1
+        });
+    }
 
     render(){
         return (
             <div>
                 <h1>Join Short Link</h1>
-
+                <div>
+                    <p> {this.state.count}</p>
+                    <span>
+                        <button onClick={this.increment.bind(this)}>+1</button>
+                        <button onClick={this.decrement.bind(this)}>-1</button>
+                    </span>
+                </div>
                 <Link to='/'>Already have an account?</Link>
             </div>
         );
