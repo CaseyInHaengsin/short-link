0e158af1baef98adeecf9b63aa7c83df6226e830
CaseyInHaengsin <casey.rowley89@gmail.com>
2018-03-29 09:58:21 -0600

    Added Meteor login function

--- imports/ui/Signup.js
+++ imports/ui/Signup.js
@@ -1,6 +1,6 @@
 import React from "react";
 import { Link } from 'react-router-dom';
-
+import {Accounts } from 'meteor/accounts-base';
 
 export default class Signup extends React.Component {
     constructor(props) {
@@ -12,9 +12,18 @@ export default class Signup extends React.Component {
 
     onSubmit(e){
         e.preventDefault();
-        this.setState({
+        let email = this.refs.email.value.trim();
+        let password = this.refs.password.value.trim();
+
+        //this is a meteor function used to create a user. The email and password are using
+        //an es6 way to assign email and passwords
+        Accounts.createUser({email, password}, (err) => {
+            console.log('Signup Callback', err);
+
+        });
+       /* this.setState({
             error: 'Failure'
-        })
+        })*/
     }
 
 
@@ -25,8 +34,8 @@ export default class Signup extends React.Component {
                 {this.state.error ? <p>{this.state.error}</p> : undefined}
 
                 <form onSubmit={this.onSubmit.bind(this)}>
-                    <input type='email' name='email' placeholder='Email'/>
-                    <input type='password' name='password' placeholder='Password'/>
+                    <input type='email' ref='email' name='email' placeholder='Email'/>
+                    <input type='password' ref='password' name='password' placeholder='Password'/>
                     <button>Create Account</button>
 
 
